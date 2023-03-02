f58a181b856ed8f79898ca402db3f8859ce45ac0
itauditresources <class.method.apply@gmail.com>
2023-03-02 12:28:14 +0100

    bugfix

--- createIndexFile.js
+++ createIndexFile.js
@@ -4,17 +4,16 @@ const path = require('path')
 let outputDir = 'js/main.js'
 
 // IIFE
-;(async () => {
-    await fs.readdir(path.join(__dirname, '/js'), (err, files) => {
+;(() => {
+    fs.readdir(path.join(__dirname, '/js'), (err, files) => {
         if (err) throw err
 
         files.sort((a, b) => {
             return a.slice(0, 1) - b.slice(0, 1)
         })
-        console.log(files)
 
         files.forEach(file => {
-            if (file.startsWith('-', 1) && file.slice('.')[1] === 'js') {
+            if (file.startsWith('-', 1) && file.split('.')[1] === 'js') {
                 console.log(file)
                 fs.readFile(path.join(__dirname, `js/${file}`), (err, data) => {
                     if (err) throw err
